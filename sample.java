90c88d9829ecfc4ca4a7e8d0a47cfa08bd0c9325
malvinoT <71373022+malvinoT@users.noreply.github.com>
2020-09-16 16:23:35 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -7,8 +7,6 @@ public class HelloGuys {
 		System.out.println("Hey Duck");
 		System.out.println("Hey Arya");//newly added
 		System.out.println("Hey vino");//newly added
-		System.out.println("Hey mal");//newly added
-		System.out.println("Hey al");//newly added
 	}
 
 }
