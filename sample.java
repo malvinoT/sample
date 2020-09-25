efde5f1feeee1418d47e91ace2b9f8b664aa6e38
malvinoT <71373022+malvinoT@users.noreply.github.com>
2020-09-25 18:40:33 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -7,7 +7,6 @@ public class HelloGuys {
 		System.out.println("Hey Duck");
 		System.out.println("Hey Arya");//newly added
 		System.out.println("Hey san");//newly added
-		System.out.println("Hey sa//newly added
 	}
 
 }
