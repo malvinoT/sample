07ff0cd4c324f4e76eb2b20e2049f8e733367084
malvinoT <71373022+malvinoT@users.noreply.github.com>
2020-10-21 11:29:27 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,7 +2,7 @@ public class HelloGuys {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		System.out.println("hello world");
 		System.out.println("malvin");
 	}
 
