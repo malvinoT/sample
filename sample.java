227eb6b2d0d55f198a50a8d917ac1ad7387edf25
malvinoT <71373022+malvinoT@users.noreply.github.com>
2020-10-21 11:28:03 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,11 +2,8 @@ public class HelloGuys {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hey Agni");
-		System.out.println("Hey Riya");
-		System.out.println("Hey Duck");
-		System.out.println("Hey Arya");//newly added
-		System.out.println("Hey san");//newly added
+		System.out.println("hello");
+		System.out.println("malvin");
 	}
 
 }
